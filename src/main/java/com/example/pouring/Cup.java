c2defa24ac5eb4293a3250b4d99933f91d9439e3
Alex Verri <alex.verri@webskunk.it>
2024-01-25 17:07:37 -0500

    fixed cup bean

--- src/main/java/com/example/pouring/Cup.java
+++ src/main/java/com/example/pouring/Cup.java
@@ -1,17 +1,13 @@
 package com.example.pouring;
 
-public class Cup {
-    private final String size;
-    private final String name;
-    private final int defaultRate;
-    private final int quantity;
-    private final int price;
+import lombok.Data;
 
-    public Cup(String size, String name, int defaultRate, int quantity, int price) {
-        this.size = size;
-        this.name = name;
-        this.defaultRate = defaultRate;
-        this.quantity = quantity;
-        this.price = price;
-    }
+@Data
+public class Cup {
+    private String size;
+    private String name;
+    private int defaultRate;
+    private int quantity;
+    private int price;
+
 }
